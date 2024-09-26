a0964d62dc9dd0599fc50ebcb71ac9cf29130b0f
NGANDJUI NANGA Ivan Delors <ngandjui.ivan@institutsaintjean.org>
2024-09-26 19:17:57 +0200

    Creation du bouton de menu gauche

--- tp1_jubi_auto/src/app/app.component.ts
+++ tp1_jubi_auto/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonMenu, IonRouterOutlet } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, IonMenu],
 })
 export class AppComponent {
   constructor() {}
